86418ab59e3ad2dd9ffa1e1352523e016c34dcf0
axcamz <acamaulana123@gmail.com>
2022-06-06 20:18:03 +0700

    deleted previous function

--- pages/[chapter].jsx
+++ pages/[chapter].jsx
@@ -22,34 +22,10 @@ export default function Surah(){
             const chapter_data = await fetchData(`https://api.quran.com/api/v4/chapters/${id}`)
             const surah_info = await fetchData(`https://api.quran.com//api/v4/chapters/${id}/info?language=id`)
             const verses = await fetchData(`https://api.quran.com/api/v4/verses/by_chapter/${id}?language=id&fields=text_uthmani&translation_fields=resource_name,language_id&translations=33&per_page=220`)
-            await setData({...chapter_data, ...surah_info, ...verses})
-            await setLoading(false)
+            setData({...chapter_data, ...surah_info, ...verses})
+            setLoading(false)
         }
 
-        function getChapterData(chapterId){
-            fetch(`https://api.quran.com/api/v4/chapters/${chapterId}`)
-            .then((res) => res.json())
-            .then((data) => {
-                setData(data)
-            })
-        }
-
-        function getSurahInfo(chapterId){
-            fetch(`https://api.quran.com//api/v4/chapters/${chapterId}/info?language=id`)
-            .then((res) => res.json())
-            .then((data) => {
-                setData((datas) => {return {...datas, ...data}})
-            })
-        }
-
-        function getVersesOfChapter(chapterId){
-            fetch(`https://api.quran.com/api/v4/verses/by_chapter/${chapterId}?language=id&fields=text_uthmani&translation_fields=resource_name,language_id&translations=33&per_page=220`)
-            .then((res) => res.json())
-            .then((data) => {
-                setData((datas) => {return {...datas, ...data}})
-            })
-            .then(() => setLoading(false))
-        }
 
         if(router.isReady){
             getData(router.query.chapter)
