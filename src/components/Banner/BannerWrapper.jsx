c712a95add75d0ffe8e39102f76e998e3adf2ce5
axcamz <acamaulana123@gmail.com>
2022-06-10 21:43:50 +0700

    fix style in Banner Components

--- src/components/Banner/BannerWrapper.jsx
+++ src/components/Banner/BannerWrapper.jsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 
 const BannerWrapper = ({children, imageOpacity, imageScale}) => {
   return (
-    <div className='rounded-2xl p-4 w-full bg-gradient-to-tr overflow-hidden from-emerald-300 to-emerald-700 relative shadow-lg shadow-emerald-200 transition-all'>
+    <div className='rounded-2xl p-4 w-full lg:p-6 lg:mb-12 bg-gradient-to-tr overflow-hidden from-emerald-300 to-emerald-700 relative shadow-lg shadow-emerald-200 transition-all'>
         {children}
         <div style={{opacity: imageOpacity, transform: `scale(${imageScale})`}} className='absolute top-2 right-0 w-44 h-32'>
             <Image
--- src/components/Banner/ChapterBanner.jsx
+++ src/components/Banner/ChapterBanner.jsx
@@ -13,15 +13,15 @@ const ChapterBanner = ({chapterData, chapterInfo, isLoading}) => {
                 isLoading ? 
                 <div>Loading</div>
                 :
-                <div className={`text-center z-20 relative text-gray-50 duration-150 transition-all ${isInfoOpen ? 'max-h-96':'max-h-32'}`}>
+                <div className={`text-center z-20 relative text-gray-50 duration-150 ${isInfoOpen ? 'max-h-96':'max-h-32'}`}>
                     <Info
-                        className="h-5 w-5 absolute left-0 top-0"
+                        className="h-5 lg:h-8 cursor-pointer absolute left-0 top-0 text-gray-50"
                         onClick={() => setInfoOpen(!isInfoOpen)}
                     />
-                    <Bookmark className="h-5 w-5 absolute right-0 top-0"/>
-                    <h1 className='text-2xl font-bold mb-0 text-gray-50'>{chapterData.name_simple}</h1>
-                    <span className='text-sm block'>{chapterData.translated_name.name}</span>
-                    <hr  className='my-3'/>
+                    <Bookmark className="h-5 cursor-pointer lg:h-8 absolute right-0 top-0 text-gray-50"/>
+                    <h1 className='text-2xl lg:text-3xl font-bold mb-0 text-gray-50'>{chapterData.name_simple}</h1>
+                    <span className='text-sm block lg:text-xl text-gray-50'>{chapterData.translated_name.name}</span>
+                    <hr  className={`my-3 mx-auto lg:my-5 md:transition-all ${isInfoOpen ? 'max-w-full':'max-w-md'}`}/>
                     <SurahInfo
                         verses_count={chapterData.verses_count}
                         revelation_place={chapterData.revelation_place}
@@ -29,7 +29,7 @@ const ChapterBanner = ({chapterData, chapterInfo, isLoading}) => {
                         className={`${isInfoOpen ? 'max-h-96 visible opacity-100':'max-h-0 invisible opacity-0'}`}
                         chapterId={chapterData.id}
                     />
-                    <span className={`text-sm transition-all ${isInfoOpen ? 'invisible opacity-0':'visible opacity-100'}`}><span className='capitalize'>{chapterData.revelation_place}</span> - {chapterData.verses_count} Ayah</span>
+                    <span className={`text-sm transition-all lg:text-lg text-gray-50 ${isInfoOpen ? 'invisible opacity-0':'visible opacity-100'}`}><span className='capitalize'>{chapterData.revelation_place}</span> - {chapterData.verses_count} Ayah</span>
                 </div>
             }
         </BannerWrapper>
