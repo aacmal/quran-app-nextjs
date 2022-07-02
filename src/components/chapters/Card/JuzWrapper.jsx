94c08716c47beb208f60782fe3c952ff09d660d5
Aca M <61030878+axcamz@users.noreply.github.com>
2022-07-02 11:39:51 +0700

    Fix position number juz to sticky

--- src/components/chapters/Card/JuzWrapper.jsx
+++ src/components/chapters/Card/JuzWrapper.jsx
@@ -6,7 +6,7 @@ const JuzWrapper = ({children, juz_number}) => {
     <Link href={`/juz/${juz_number}`}>
       <a className='h-fit col-span-3'>
         <div className="p-3 bg-emerald-200 dark:bg-emerald-700 gap-3 lg:p-6 grid rounded-xl h-fit border border-transparent hover:border-emerald-500 dark:hover:border-emerald-400 outline-none">
-            <span className='group-hover:underline decoration-emerald-500 font-bold text-emerald-600 dark:text-emerald-400'>Juz {juz_number}</span>
+            <span className='group-hover:underline decoration-emerald-500 font-bold text-emerald-600 dark:text-emerald-400 sticky top-0 w-full z-30 py-px bg-emerald-200 dark:bg-emerald-700'>Juz {juz_number}</span>
             {children}
         </div>
       </a>
