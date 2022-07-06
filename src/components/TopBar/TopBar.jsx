3a02934e8e2855f42d70a8a6589f2d63689cf1ea
Aca M <61030878+axcamz@users.noreply.github.com>
2022-07-06 20:56:35 +0700

    Add Search feature & fix method when hide topbar

--- src/components/TopBar/TopBar.jsx
+++ src/components/TopBar/TopBar.jsx
@@ -21,12 +21,9 @@ const TopBar = () => {
 
 	}, [])
 
-	if(!isLoading){
+	if(!isLoading && showTopbar){
 		return (
-			<div className={classNames('fixed top-0 bg-gray-100 dark:bg-slate-700 border-b border-emerald-500/50 shadow-lg shadow-emerald-700/10 w-full left-0 z-50 md:py-3  2xl:px-32 md:px-8 px-3 py-2 transition-all justify-between', 
-				{"flex" : showTopbar},
-				{"hidden" : !showTopbar}
-			)}>
+			<div className={classNames('fixed top-0 bg-gray-100 dark:bg-slate-700 border-b border-emerald-500/50 shadow-lg shadow-emerald-700/10 w-full left-0 z-50 md:py-3  2xl:px-32 md:px-8 px-3 py-2 transition-all justify-between flex')}>
 				<DropdownSurahLists chapterLists={allChapters} chapterActive={currentChapter}/>
 				<DeveloperUtility isInSurah={true}/>
 			</div>
