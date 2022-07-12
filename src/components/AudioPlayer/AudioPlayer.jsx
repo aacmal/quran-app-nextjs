855d7fbc7b09c525649c1f35465282f2f9972dd4
Aca M <61030878+axcamz@users.noreply.github.com>
2022-07-12 08:45:09 +0700

    update currentTime when slider change

--- src/components/AudioPlayer/AudioPlayer.jsx
+++ src/components/AudioPlayer/AudioPlayer.jsx
@@ -13,7 +13,7 @@ const AudioPlayer = () => {
 	const intervalRef = useRef()
 
 	// Control State
-	const [trackIndex, setTrackIndex] = useState(0);
+	const [value, setValue] = useState(0);
 	const [trackProgress, setTrackProgress] = useState(0);
 	const [isPlaying, setIsPlaying] = useState(true);
 
@@ -29,20 +29,24 @@ const AudioPlayer = () => {
 	}
 
 	function updateCurrentTime(value){
+		setTrackProgress(Math.floor(value))
 		setCurrentTime(calculateTime(value))
+		setValue(value)
 	}
 
 	function startTimer(){
 		clearInterval(intervalRef.current)
 
 		intervalRef.current = setInterval(() => {
-			updateCurrentTime(audioRef.current.currentTime)
-			setTrackProgress(Math.floor(audioRef.current.currentTime))
+			setCurrentTime(calculateTime(Math.floor(audioRef.current.currentTime)+1))
+			setTrackProgress(Math.floor(audioRef.current.currentTime)+1)
 		}, [1000])
 	}	
 
 	useEffect(() => {
-	}, [audioData])
+		console.log(audioRef.current.duration);
+		setMaxtime(audioRef.current.duration)
+	}, [])
 
 	useEffect(() => {
 		if(isPlaying){
@@ -58,14 +62,26 @@ const AudioPlayer = () => {
 	function handleOnLoad(value){
 		setTrackProgress(0)
 		updateCurrentTime(0)
-		setIsPlaying(!isPlaying)
+		setIsPlaying(false)
 		setMaxtime(value)
+		console.log(maxTime);
+	}
+
+	function handleUpdate(){
+		audioRef.current.currentTime = value
 	}
 
 	return (
 		<div className='fixed bottom-0 py-2 dark:bg-slate-600 bg-white shadow-lg shadow-black w-full'>
 			<Wrapper className="py-0 pt-0 m-0">
-				<audio style={{display: 'none'}} onLoadedMetadata={(e) => handleOnLoad(e.target.duration)} ref={audioRef} src={`https://download.quranicaudio.com/qdc/mishari_al_afasy/murattal/${audioData}.mp3`} preload='metadata' loop={true}/>
+				<audio 
+					style={{display: 'none'}} 
+					onLoadedMetadata={(e) => handleOnLoad(e.target.duration)} 
+					ref={audioRef} 
+					src={`https://download.quranicaudio.com/qdc/mishari_al_afasy/murattal/${audioData}.mp3`} 
+					preload='metadata' 
+					loop={true}
+				/>
 
 				<div className='flex'>
 					<PlayIcon onClick={() => setIsPlaying(!isPlaying)} className="h-10 cursor-pointer"/>
@@ -78,6 +94,7 @@ const AudioPlayer = () => {
 							onChange={(e) => updateCurrentTime(e.target.value)}
 							max={maxTime}
 							value={trackProgress}
+							onPointerUp={handleUpdate}
 						/>
 						<span>{calculateTime(maxTime)}</span>
 					</div>
