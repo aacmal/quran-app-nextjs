b1e52a39eeba3778f028cb7d423bea4340bbf757
Aca M <61030878+axcamz@users.noreply.github.com>
2022-08-15 20:46:55 +0700

    Edit padding header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,7 +3,7 @@ import DeveloperUtility from "../TopBar/DeveloperUtility/DeveloperUtility"
 
 const Header = ({children, className}) => {
   return (
-    <div className="flex lg:justify-between lg:items-center lg:flex-row flex-col px-5">
+    <div className="flex lg:justify-between lg:items-center lg:flex-row flex-col px-5 xl:px-0">
       <div className="w-full flex justify-between lg:pr-5">
         <h1 className={"text-xl font-bold text-emerald-500 "+ className}>{children}</h1>
         <DeveloperUtility/>
