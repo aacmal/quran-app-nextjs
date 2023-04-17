df5335887c714750076dda4488012902dc808405
Aca M <acamaulana123@gmail.com>
2023-04-18 05:15:57 +0700

    added ads script

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,7 @@ class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com"/>
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
           <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700;900&display=swap" rel="stylesheet"/>
+          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6233051846019912" crossOrigin="anonymous"></script>
         </Head>
         <body className='white dark:bg-slate-800'>
           <Main />
