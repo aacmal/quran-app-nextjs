475a6766fc0560cf4392c6f3dd11db71d504d463
Aca M <acamaulana123@gmail.com>
2023-06-10 00:46:15 +0700

    remove locale and middleware

--- next.config.js
+++ next.config.js
@@ -24,15 +24,15 @@ const nextConfig = {
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
-  i18n: {
-    localeDetection: false,
-    // These are all the locales you want to support in
-    // your application
-    locales: ['id', 'en'],
-    // This is the default locale you want to be used when visiting
-    // a non-locale prefixed path e.g. `/hello`
-    defaultLocale: 'id',
-  },
+  // i18n: {
+  //   localeDetection: false,
+  //   // These are all the locales you want to support in
+  //   // your application
+  //   locales: ['id', 'en'],
+  //   // This is the default locale you want to be used when visiting
+  //   // a non-locale prefixed path e.g. `/hello`
+  //   defaultLocale: 'id',
+  // },
 }
 
 module.exports = {
